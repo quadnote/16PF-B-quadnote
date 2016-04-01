10644a9f44ad7c394cceb162b50a01ad264630ce
CAD Client <CAD Client>
2016-04-01 15:53:19 +0900

    ex06. Study drills 1. 주석 삽입

--- ex06/ex06.py
+++ ex06/ex06.py
@@ -1,12 +1,17 @@
+#-*-coding:cp949
+# http://learnpythonthehardway.org/book/ex6.html
 x = "There are %d types of people." % 10
+# %d 는 정수부분 표시
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary, do_not)
+# #s 는 문자열 표시
 
 print x
 print y
 
 print "I said: %r." % x
+# %r 은...
 print "I also said: '%s'." % y
 
 hilarious = False
