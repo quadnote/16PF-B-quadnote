9d96ef8f67469dbdbbbe3431ef221cf1c7aa813a
CAD Client <CAD Client>
2016-04-01 15:00:05 +0900

    Study Drills 05.01변수 명 변경 My 삭제

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,18 +1,18 @@
 name = "Zed A. Shaw"
-my_age = 35 # not a lie
-my_height_cm = 74
-my_weight_kg = 180
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Black'
+age = 35 # not a lie
+height_cm = 74
+weight_kg = 180
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
 
 print "Let's talk about %s." % name
-print "He's %d cm tall." % my_height_cm
-print "He's %d kg heavy." % my_weight_kg
+print "He's %d cm tall." % height_cm
+print "He's %d kg heavy." % weight_kg
 print "Accually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height_cm, my_weight_kg, my_age + my_height_cm + my_weight_kg)
+    age, height_cm, weight_kg, age + height_cm + weight_kg)
