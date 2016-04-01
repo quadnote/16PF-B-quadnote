0b577eb05bd2c8b788a5acbc4d531198efb6835d
CAD Client <CAD Client>
2016-04-01 14:52:05 +0900

    ex05 변수 단위 변경

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,18 +1,18 @@
 my_name = "Zed A. Shaw"
 my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # 1bs
-my_eyes = 'Blue'
+my_height_cm = 74
+my_weight_kg = 180
+my_eyes = 'Brown'
 my_teeth = 'White'
-my_hair = 'Brown'
+my_hair = 'Black'
 
 print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "He's %d cm tall." % my_height_cm
+print "He's %d kg heavy." % my_weight_kg
 print "Accually that's not too heavy."
 print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight)
+    my_age, my_height_cm, my_weight_kg, my_age + my_height_cm + my_weight_kg)
