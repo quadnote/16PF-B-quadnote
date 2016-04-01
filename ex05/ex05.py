da88f7aaef714e7507ad889e0b7d3ee69442bc25
CAD Client <CAD Client>
2016-04-01 15:10:12 +0900

    Study Drills 05.02 단위 변환

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,18 +1,17 @@
-name = "Zed A. Shaw"
-age = 35 # not a lie
-height_cm = 74
-weight_kg = 180
-eyes = 'Brown'
-teeth = 'White'
-hair = 'Black'
-
+name = 'Ted'
+my_age = 46 # It's a lie
+my_height_cm = 180.5
+cm_to_inch = 1.0 / 2.54
+my_height_inch = my_height_cm * cm_to_inch
+my_weight_kg = 80
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+#place holder
 print "Let's talk about %s." % name
-print "He's %d cm tall." % height_cm
-print "He's %d kg heavy." % weight_kg
-print "Accually that's not too heavy."
-print "He's got %s eyes and %s hair." % (eyes, hair)
-print "His teeth are usually %s depending on the coffee." % teeth
-
-# this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (
-    age, height_cm, weight_kg, age + height_cm + weight_kg)
+print "He's %g centimeters tall." % my_height_cm
+print "He's %g inches tall." % my_height_inch
+print "He's %d kilograms heavy." % my_weight_kg
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
