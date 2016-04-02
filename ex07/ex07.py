1fda2b1c7f60fe73f1f4e9eefbc3d03ff16156ab
CAD Client <CAD Client>
2016-04-02 10:04:28 +0900

    ex07. print 문구 뒤 쉼표의 쓰임새

--- ex07/ex07.py
+++ ex07/ex07.py
@@ -1,3 +1,4 @@
+#-*-coding:cp949
 print "Mary had a little lamb."
 print "Its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
@@ -19,3 +20,4 @@ end12 = "r"
 # whatch that comma at the end. try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
+# 프린트 문구 뒤에 반점을 넣을시 그 밑줄에 있는 print 문구가 한 칸을 띄우고 이어서 따라붙음.
