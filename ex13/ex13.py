2dd0a6121ae66287e961b3e7a7a36a0eb2644b90
CAD Client <CAD Client>
2016-04-08 16:15:21 +0900

    added ex13.ex14.

--- ex13/ex13.py
+++ ex13/ex13.py
@@ -1,6 +1,8 @@
 # http://learnpythonthehardway.org/book/ex13.html
+#-*-coding:cp949
 
 from sys import argv
+#sys 모듈(파이썬 스크립트에 전달된 명령행 매개변수 목록)에서 argv( 를 가져다가 쓰는 것
 
 print("argv = " + str(argv))
 script, first, second, third = argv
