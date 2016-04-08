423a72a5daaef240c1f61d6689a9fe6953acd6f6
CAD Client <CAD Client>
2016-04-08 15:35:54 +0900

    ex13. Parameters, Unpacking, Variables

---
+++ ex13/ex13.py
@@ -0,0 +1,11 @@
+# http://learnpythonthehardway.org/book/ex13.html
+
+from sys import argv
+
+print("argv = " + str(argv))
+script, first, second, third = argv
+
+print ("The script is called: %s" % script)
+print ("Your first variable is: %s" % first)
+print ("Your second variable is: %s" % second)
+print ("Your third variable is: %s" % third)
