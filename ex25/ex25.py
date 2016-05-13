bfa487106cff53d7d3a8a09004ea5c7f0d6b3ee2
CAD Client <CAD Client>
2016-05-13 15:10:45 +0900

    ex25

--- ex25/ex25.py
+++ ex25/ex25.py
@@ -20,7 +20,7 @@ def print_last_word(words):
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
-    return sort_words(words)
+    return sort_words(words)sentecne
 
 def print_first_and_last(sentence):
     """Prints the first and last words of the sentence."""
