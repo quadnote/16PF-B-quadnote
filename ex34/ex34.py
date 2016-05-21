a49d180d7e872dd05b524d0122d965049a835826
CAD Client <CAD Client>
2016-05-21 10:18:00 +0900

    ex34-fix

---
+++ ex34/ex34.py
@@ -0,0 +1,13 @@
+#-*-coding:cp949
+
+animals = ['곰', '호랑이', '펭귄', '얼룩말']
+bear = animals[0]
+tiger = animals[1]
+penguin = animals[2]
+zebra = animals[3]
+# 리스트가 시작할 때 0부터 시작한다.
+
+print "bear= %s" % bear
+print "tiger= %s" % tiger
+print "penguin= %s" % penguin
+print "zebra= %s" % zebra
